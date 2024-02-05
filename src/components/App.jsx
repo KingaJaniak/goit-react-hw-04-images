4510c184d13067d12ee1913d565e92d490bdfbd0
Kinga Janiak <janiak.kinga96@gmail.com>
2024-02-05 18:27:37 +0100

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react';
 import axios from 'axios';
-import { Component } from 'react';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import ImageGalleryItem from './ImageGalleryItem';
@@ -11,79 +11,77 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 const apiKey = '41181454-8b56e64d19cc61326c145b8db';
 const perPage = 12;
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: [],
-      loading: false,
-      showModal: false,
-      selectedImage: null,
-      activePage: 1,
-    };
-  }
-  handleSearch = async query => {
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [query, setQuery] = useState('');
+
+  const handleSearch = async (query) => {
     try {
-      this.setState({ loading: true, query });
+      setLoading(true);
+      setQuery(query);
       const response = await axios.get(
         `?q=${query}&page=1&key=${apiKey}&per_page=${perPage}`
       );
-      this.setState({ images: response.data.hits, loading: false });
+      setImages(response.data.hits);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching images:', error);
-      this.setState({ loading: false });
+      setLoading(false);
     }
-  };
-  handleLoadMore = async () => {
-    try {
-      this.setState({ loading: true });
-      const nextPage = Math.ceil(this.state.images.length / perPage) + 1;
-      const response = await axios.get(
-        `?q=${this.state.query}&page=${nextPage}&key=${apiKey}&per_page=${perPage}`
-      );
-      const newImages = response.data.hits;
-      this.setState(prevState => ({
-        images: [...prevState.images, ...newImages],
-        loading: false,
-      }));
-    } catch (error) {
-      console.error('Error fetching more images:', error);
-      this.setState({ loading: false });
-    }
-  };
-  handleImageClick = imageURL => {
-    this.setState({ showModal: true, selectedImage: imageURL });
   };
 
-  handleCloseModal = () => {
-    this.setState({ showModal: false, selectedImage: null });
+  const handleLoadMore = async () => {
+    try {
+      setLoading(true);
+      const nextPage = Math.ceil(images.length / perPage) + 1;
+      const response = await axios.get(
+        `?q=${query}&page=${nextPage}&key=${apiKey}&per_page=${perPage}`
+      );
+      const newImages = response.data.hits;
+      setImages((prevImages) => [...prevImages, ...newImages]);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching more images:', error);
+      setLoading(false);
+    }
   };
-  render() {
-    return (
-      <AppWrapper>
-        <Searchbar onSubmit={this.handleSearch} />
-        <ImageGallery>
-          {this.state.images.map((image, index) => (
-            <ImageGalleryItem key={`${image.id}-${index}`}>
-              <ImgSmall
-                src={image.webformatURL}
-                alt=""
-                onClick={() => this.handleImageClick(image.largeImageURL)}
-              />
-            </ImageGalleryItem>
-          ))}
-        </ImageGallery>
-        {this.state.loading && <Loader />}
-        {this.state.images.length > 0 && (
-          <BtnLoader onClick={this.handleLoadMore}>Load More</BtnLoader>
-        )}
-        {this.state.showModal && (
-          <Modal
-            imageURL={this.state.selectedImage}
-            onClose={this.handleCloseModal}
-          />
-        )}
-      </AppWrapper>
-    );
-  }
-}
+
+  const handleImageClick = (imageURL) => {
+    setShowModal(true);
+    setSelectedImage(imageURL);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedImage(null);
+  };
+
+  return (
+    <AppWrapper>
+      <Searchbar onSubmit={handleSearch} />
+      <ImageGallery>
+        {images.map((image, index) => (
+          <ImageGalleryItem key={`${image.id}-${index}`}>
+            <ImgSmall
+              src={image.webformatURL}
+              alt=""
+              onClick={() => handleImageClick(image.largeImageURL)}
+            />
+          </ImageGalleryItem>
+        ))}
+      </ImageGallery>
+      {loading && <Loader />}
+      {images.length > 0 && (
+        <BtnLoader onClick={handleLoadMore}>Load More</BtnLoader>
+      )}
+      {showModal && (
+        <Modal imageURL={selectedImage} onClose={handleCloseModal} />
+      )}
+    </AppWrapper>
+  );
+};
+
+export default App;
